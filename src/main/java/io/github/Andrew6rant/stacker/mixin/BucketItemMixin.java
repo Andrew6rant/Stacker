c37cdedc369b40b7df63d68e2841390e1d9ea2b2
Andrew6rant <57331134+Andrew6rant@users.noreply.github.com>
2021-08-11 18:27:19 -0400

    Bucket Mixin

---
+++ src/main/java/io/github/Andrew6rant/stacker/mixin/BucketItemMixin.java
@@ -0,0 +1,23 @@
+package io.github.Andrew6rant.stacker.mixin;
+
+import io.github.Andrew6rant.stacker.util.ItemsHelper;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BucketItem.class)
+public class BucketItemMixin {
+
+    @Inject(method = "getEmptiedStack", at = @At(value = "HEAD"), cancellable = true)
+    private static void stackableBucket(ItemStack stack, PlayerEntity player, CallbackInfoReturnable<ItemStack> cir){
+        ItemsHelper.insertNewItem(player, new ItemStack(Items.BUCKET));
+        stack.decrement(1);
+        cir.setReturnValue(stack);
+    }
+
+}
