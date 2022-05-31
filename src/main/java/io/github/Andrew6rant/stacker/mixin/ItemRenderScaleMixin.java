aec30a76337e61b7b561cbde9ab2cd19d0e48028
Andrew Grant <andrew.grant@mail.rit.edu>
2022-05-31 14:21:14 -0400

    compile using Java 17

--- src/main/java/io/github/Andrew6rant/stacker/mixin/ItemRenderScaleMixin.java
+++ src/main/java/io/github/Andrew6rant/stacker/mixin/ItemRenderScaleMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-//@Environment(EnvType.CLIENT)
+@Environment(EnvType.CLIENT)
 @Pseudo
 @Mixin(targets = "net.devtech.stacc.ItemCountRenderHandler")
 public class ItemRenderScaleMixin {
