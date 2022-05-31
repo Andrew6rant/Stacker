83b9a5665e2b5f5c0627d561a4c860785c4217e4
Andrew Grant <andrew.grant@mail.rit.edu>
2022-05-31 14:14:04 -0400

    update to 1.18+

--- src/main/java/io/github/Andrew6rant/stacker/mixin/StewItemMixin.java
+++ src/main/java/io/github/Andrew6rant/stacker/mixin/StewItemMixin.java
@@ -5,15 +5,15 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.item.MushroomStewItem;
+import net.minecraft.item.StewItem;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MushroomStewItem.class)
-public class StewItemMixin { //This mixin works for soup as well
+@Mixin(StewItem.class)
+public class StewItemMixin { //This mixin works for soups as well
     @Inject(method = "finishUsing", at=@At(value = "NEW", target = "net/minecraft/item/ItemStack"), cancellable = true)
     private void stackableStew(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir){
         ItemsHelper.insertNewItem((PlayerEntity)user, new ItemStack(Items.BOWL));
